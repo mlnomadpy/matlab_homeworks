b06e1eea8e3d09a5eb3629b97595216c2699e93d
Taha Bouhsine <45195299+mlnomadpy@users.noreply.github.com>
2024-09-27 14:16:56 +0000

    I solved the first exo in the assignment, horay!!!

---
+++ assignment_1/exo1.m
@@ -0,0 +1,16 @@
+% Author Name: Taha Bouhsine    
+% Email: tahabhs14@gmail.com   
+% Course: MATLAB Programming - Fall 2024
+% Assignment: Homework 1
+% Exo 1
+
+% Creating Variablees
+r = 5 % Radius
+% Calculating the Area of the circle
+a =  %Area
+% Caclulating the Circumeference of the CIrcle
+c = %circumference
+% Printing the results
+fprintf('The radius of our circle is %f \n', r)
+fprintf('area is %f \n', a)
+fprintf('circumference is %f \n', c)
